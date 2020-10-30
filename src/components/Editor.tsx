79a4983b4eed01537f4684c2aaef20561ae3437a
Andrew Prifer <andrewpeterprifer@gmail.com>
2020-10-30 12:25:51 +0100

    Use shadow dom to isolate editor styles

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -5,6 +5,7 @@ import { OrbitControls } from '@react-three/drei';
 import shallow from 'zustand/shallow';
 import { WebGLRenderer } from 'three';
 import { saveAs } from 'file-saver';
+import root from 'react-shadow';
 import Proxy from './Proxy';
 
 const referenceHeight = 200;
@@ -25,7 +26,7 @@ const EditorScene = () => {
 
   return (
     <>
-      <primitive object={staticSceneProxy}></primitive>
+      <primitive object={staticSceneProxy} />
 
       <directionalLight position={[10, 20, 15]} />
       <gridHelper args={[30, 30, 30]} />
@@ -91,105 +92,111 @@ const Editor = () => {
     };
   }, [open]);
 
-  return open ? (
-    <>
-      <div
-        style={{
-          position: 'fixed',
-          width: '100%',
-          height: '100%',
-          top: '0',
-          left: '0',
-          right: '0',
-          bottom: '0',
-          backgroundColor: 'rgba(0,0,0,0.5)',
-          zIndex: 1000,
-          cursor: 'pointer',
-        }}
-      >
-        {scene ? (
-          <Canvas
-            colorManagement
-            camera={{ position: [5, 5, 5] }}
-            onCreated={({ gl }) => {
-              gl.setClearColor('white');
-            }}
-            shadowMap
-          >
-            <EditorScene />
-          </Canvas>
-        ) : (
-          <div>Editor hasn't been attached.</div>
-        )}
-      </div>
-      <div
-        style={{
-          position: 'fixed',
-          zIndex: 1001,
-        }}
-      >
-        {gl && (
-          <canvas
-            ref={canvasRef}
-            width={
-              (gl.domElement.width / gl.domElement.height) * referenceHeight
-            }
-            height={referenceHeight}
+  return (
+    <root.div>
+      {open ? (
+        <>
+          <div
             style={{
-              outline: '1px solid black',
+              position: 'fixed',
+              width: '100%',
+              height: '100%',
+              top: '0',
+              left: '0',
+              right: '0',
+              bottom: '0',
+              backgroundColor: 'rgba(0,0,0,0.5)',
+              zIndex: 1000,
+              cursor: 'pointer',
             }}
-          />
-        )}
+          >
+            {scene ? (
+              <Canvas
+                colorManagement
+                camera={{ position: [5, 5, 5] }}
+                onCreated={({ gl }) => {
+                  gl.setClearColor('white');
+                }}
+                shadowMap
+              >
+                <EditorScene />
+              </Canvas>
+            ) : (
+              <div>Editor hasn't been attached.</div>
+            )}
+          </div>
+          <div
+            style={{
+              position: 'fixed',
+              zIndex: 1001,
+            }}
+          >
+            {gl && (
+              <canvas
+                ref={canvasRef}
+                width={
+                  (gl.domElement.width / gl.domElement.height) * referenceHeight
+                }
+                height={referenceHeight}
+                style={{
+                  outline: '1px solid black',
+                }}
+              />
+            )}
 
-        <button onClick={() => setOpen(false)}>Close</button>
-        <button
-          onClick={() => {
-            const blob = new Blob(
-              [
-                JSON.stringify(
-                  {
-                    editables: Object.fromEntries(
-                      Object.entries(editables).map(([name, editable]) => [
-                        name,
-                        {
-                          transform: editable.transform.toArray(),
-                        },
-                      ])
+            <button onClick={() => setOpen(false)}>Close</button>
+            <button
+              onClick={() => {
+                const blob = new Blob(
+                  [
+                    JSON.stringify(
+                      {
+                        editables: Object.fromEntries(
+                          Object.entries(editables).map(([name, editable]) => [
+                            name,
+                            {
+                              transform: editable.transform.toArray(),
+                            },
+                          ])
+                        ),
+                      },
+                      null,
+                      2
                     ),
-                  },
-                  null,
-                  2
-                ),
-              ],
-              { type: 'text/json;charset=utf-8' }
-            );
-            saveAs(blob, 'editableState.json');
+                  ],
+                  { type: 'text/json;charset=utf-8' }
+                );
+                saveAs(blob, 'editableState.json');
+              }}
+            >
+              Export
+            </button>
+            <select
+              value={transformControlsMode}
+              onChange={(event) =>
+                setTransformControlsMode(
+                  event.target.value as TransformControlMode
+                )
+              }
+            >
+              <option value="translate">Translate</option>
+              <option value="rotate">Rotate</option>
+              <option value="scale">Scale</option>
+            </select>
+          </div>
+        </>
+      ) : (
+        <button
+          style={{
+            position: 'fixed',
+            zIndex: 1000,
           }}
+          onClick={() => setOpen(true)}
         >
-          Export
+          Editor
         </button>
-        <select
-          value={transformControlsMode}
-          onChange={(event) =>
-            setTransformControlsMode(event.target.value as TransformControlMode)
-          }
-        >
-          <option value="translate">Translate</option>
-          <option value="rotate">Rotate</option>
-          <option value="scale">Scale</option>
-        </select>
-      </div>
-    </>
-  ) : (
-    <button
-      style={{
-        position: 'fixed',
-        zIndex: 1000,
-      }}
-      onClick={() => setOpen(true)}
-    >
-      Editor
-    </button>
+      )}
+    </root.div>
   );
 };
 
