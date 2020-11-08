54236a1c32e064234f0d382112e9cd4748aaf0ce
Andrew Prifer <andrewpeterprifer@gmail.com>
2020-11-08 20:30:32 +0100

    Improve "no canvas connected" UI
    Closes #24

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -4,7 +4,17 @@ import { useEditorStore } from '../store';
 import { OrbitControls } from '@react-three/drei';
 import shallow from 'zustand/shallow';
 import root from 'react-shadow/emotion';
-import { ChakraProvider, PortalManager, Button, Box } from '@chakra-ui/core';
+import {
+  ChakraProvider,
+  PortalManager,
+  Button,
+  Box,
+  Center,
+  VStack,
+  Heading,
+  Text,
+  Code,
+} from '@chakra-ui/core';
 import Proxy from './Proxy';
 import UI from './UI';
 import StaticSceneProxy from './StaticSceneProxy';
@@ -67,22 +77,47 @@ const Editor = () => {
             {editorOpen ? (
               <Box pos="fixed" top={0} bottom={0} left={0} right={0}>
                 {scene ? (
-                  <Canvas
-                    colorManagement
-                    camera={{ position: [5, 5, 5] }}
-                    onCreated={({ gl }) => {
-                      gl.setClearColor('white');
-                    }}
-                    shadowMap
-                    pixelRatio={window.devicePixelRatio}
-                    onPointerMissed={() => setSelected(null)}
-                  >
-                    <EditorScene />
-                  </Canvas>
+                  <>
+                    <Canvas
+                      colorManagement
+                      camera={{ position: [5, 5, 5] }}
+                      onCreated={({ gl }) => {
+                        gl.setClearColor('white');
+                      }}
+                      shadowMap
+                      pixelRatio={window.devicePixelRatio}
+                      onPointerMissed={() => setSelected(null)}
+                    >
+                      <EditorScene />
+                    </Canvas>
+                    <UI />
+                  </>
                 ) : (
-                  <div>Editor hasn't been attached.</div>
+                  <Center bg="white" height="100vh">
+                    <VStack spacing={5}>
+                      <Heading mb={4} colorScheme="red">
+                        No canvas has been connected
+                      </Heading>
+                      <Text>
+                        Please use <Code>{'<EditableManager />'}</Code> to
+                        connect a canvas to React Three Editable.
+                      </Text>
+                      <Code width="300px" p={3}>
+                        <pre>
+                          {
+                            '<Canvas>\n  <EditableManager /> {/* !!! */}\n</Canvas>'
+                          }
+                        </pre>
+                      </Code>
+                      <Button
+                        onClick={() => setEditorOpen(false)}
+                        colorScheme="teal"
+                      >
+                        Close
+                      </Button>
+                    </VStack>
+                  </Center>
                 )}
-                <UI />
               </Box>
             ) : (
               <Button
