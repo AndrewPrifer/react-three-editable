28970cb3a0f4dfa636b3d0a18f9fcdfc4d08f2a2
Andrew Prifer <andrewpeterprifer@gmail.com>
2020-11-09 22:41:59 +0100

    Also wrap PortalProvider with #react-three-editable-editor-root so tooltips are properly styled

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -73,69 +73,67 @@ const Editor = () => {
   return (
     <root.div>
       <ChakraProvider theme={theme}>
-        <PortalManager>
-          <Box
-            pos="relative"
-            zIndex={1000}
-            id="react-three-editable-editor-root"
-          >
-            {editorOpen ? (
-              <Box pos="fixed" top={0} bottom={0} left={0} right={0}>
-                {scene ? (
-                  <>
-                    <Canvas
-                      colorManagement
-                      camera={{ position: [5, 5, 5] }}
-                      onCreated={({ gl }) => {
-                        gl.setClearColor('white');
-                      }}
-                      shadowMap
-                      pixelRatio={window.devicePixelRatio}
-                      onPointerMissed={() => setSelected(null)}
-                    >
-                      <EditorScene />
-                    </Canvas>
-                    <UI />
-                  </>
-                ) : (
-                  <Center bg="white" height="100vh">
-                    <VStack spacing={5}>
-                      <Heading mb={4} colorScheme="red">
-                        No canvas has been connected
-                      </Heading>
-                      <Text>
-                        Please use <Code>{'<EditableManager />'}</Code> to
-                        connect a canvas to React Three Editable.
-                      </Text>
-                      <Code width="300px" p={3}>
-                        <pre>
-                          {
-                            '<Canvas>\n  <EditableManager /> {/* !!! */}\n</Canvas>'
-                          }
-                        </pre>
-                      </Code>
-                      <Button
-                        onClick={() => setEditorOpen(false)}
-                        colorScheme="teal"
+        <Box id="react-three-editable-editor-root">
+          <PortalManager>
+            <Box pos="relative" zIndex={1000}>
+              {editorOpen ? (
+                <Box pos="fixed" top={0} bottom={0} left={0} right={0}>
+                  {scene ? (
+                    <>
+                      <Canvas
+                        colorManagement
+                        camera={{ position: [5, 5, 5] }}
+                        onCreated={({ gl }) => {
+                          gl.setClearColor('white');
+                        }}
+                        shadowMap
+                        pixelRatio={window.devicePixelRatio}
+                        onPointerMissed={() => setSelected(null)}
                       >
-                        Close
-                      </Button>
-                    </VStack>
-                  </Center>
-                )}
-              </Box>
-            ) : (
-              <Button
-                pos="fixed"
-                bottom="20px"
-                left="20px"
-                onClick={() => setEditorOpen(true)}
-              >
-                Editor
-              </Button>
-            )}
-          </Box>
-        </PortalManager>
+                        <EditorScene />
+                      </Canvas>
+                      <UI />
+                    </>
+                  ) : (
+                    <Center bg="white" height="100vh">
+                      <VStack spacing={5}>
+                        <Heading mb={4} colorScheme="red">
+                          No canvas has been connected
+                        </Heading>
+                        <Text>
+                          Please use <Code>{'<EditableManager />'}</Code> to
+                          connect a canvas to React Three Editable.
+                        </Text>
+                        <Code width="300px" p={3}>
+                          <pre>
+                            {
+                              '<Canvas>\n  <EditableManager /> {/* !!! */}\n</Canvas>'
+                            }
+                          </pre>
+                        </Code>
+                        <Button
+                          onClick={() => setEditorOpen(false)}
+                          colorScheme="teal"
+                        >
+                          Close
+                        </Button>
+                      </VStack>
+                    </Center>
+                  )}
+                </Box>
+              ) : (
+                <Button
+                  pos="fixed"
+                  bottom="20px"
+                  left="20px"
+                  onClick={() => setEditorOpen(true)}
+                >
+                  Editor
+                </Button>
+              )}
+            </Box>
+          </PortalManager>
+        </Box>
       </ChakraProvider>
     </root.div>
   );
