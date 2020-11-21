b1ff91eb3b1de3b2cf227dbff326ff29f6844383
Andrew Prifer <andrewpeterprifer@gmail.com>
2020-11-21 13:08:14 +0100

    Revert trying to drop Editor from production bundle

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Editor from './components/Editor';
 export {
   default as EditableManager,
   EditableManagerProps,
@@ -9,8 +10,6 @@ export { default as editable } from './components/editable';
 export { EditableState, configure } from './store';
 
 if (process.env.NODE_ENV === 'development') {
-  const Editor = require('./components/Editor').default;
-
   const editorRoot = document.createElement('div');
   document.body.appendChild(editorRoot);
 
