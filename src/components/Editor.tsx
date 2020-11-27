c0dc95528c35ecaf3dbf39ee6e76a67046affde6
Andrew Prifer <andrewpeterprifer@gmail.com>
2020-11-27 14:15:29 +0100

    Apply editor shadow-dom specific base styles

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -83,92 +83,98 @@ const Editor: VFC = () => {
 
   return (
     <root.div>
-      <PortalManager>
-        <IdProvider>
-          <div className="relative z-50">
-            <div className={`fixed ${editorOpen ? 'block' : 'hidden'} inset-0`}>
-              {scene ? (
-                <>
-                  <Canvas
-                    colorManagement
-                    camera={{ position: [5, 5, 5] }}
-                    onCreated={({ gl }) => {
-                      gl.setClearColor('white');
-                    }}
-                    shadowMap
-                    pixelRatio={window.devicePixelRatio}
-                    onPointerMissed={() => setSelected(null)}
-                  >
-                    <EditorScene />
-                  </Canvas>
-                  <UI />
-                </>
-              ) : (
-                <div className="flex justify-center items-center bg-white h-screen">
-                  <div className="flex flex-col gap-5 items-center ">
-                    <Heading className="mb-4">No canvas connected</Heading>
-                    <p>
-                      Please use <Code>{'<EditableManager />'}</Code> to connect
-                      a canvas to React Three Editable.
-                    </p>
-                    <Code block>
-                      {'<Canvas>\n  <EditableManager /> {/* !!! */}\n</Canvas>'}
-                    </Code>
-                    <Button
-                      className=""
-                      onClick={() => {
-                        setEditorOpen(false);
+      <div id="react-three-editable-editor-root">
+        <PortalManager>
+          <IdProvider>
+            <div className="relative z-50">
+              <div
+                className={`fixed ${editorOpen ? 'block' : 'hidden'} inset-0`}
+              >
+                {scene ? (
+                  <>
+                    <Canvas
+                      colorManagement
+                      camera={{ position: [5, 5, 5] }}
+                      onCreated={({ gl }) => {
+                        gl.setClearColor('white');
                       }}
+                      shadowMap
+                      pixelRatio={window.devicePixelRatio}
+                      onPointerMissed={() => setSelected(null)}
                     >
-                      Close
-                    </Button>
+                      <EditorScene />
+                    </Canvas>
+                    <UI />
+                  </>
+                ) : (
+                  <div className="flex justify-center items-center bg-white h-screen">
+                    <div className="flex flex-col gap-5 items-center ">
+                      <Heading className="mb-4">No canvas connected</Heading>
+                      <p>
+                        Please use <Code>{'<EditableManager />'}</Code> to
+                        connect a canvas to React Three Editable.
+                      </p>
+                      <Code block>
+                        {
+                          '<Canvas>\n  <EditableManager /> {/* !!! */}\n</Canvas>'
+                        }
+                      </Code>
+                      <Button
+                        className=""
+                        onClick={() => {
+                          setEditorOpen(false);
+                        }}
+                      >
+                        Close
+                      </Button>
+                    </div>
                   </div>
-                </div>
+                )}
+              </div>
+              {editorOpen || (
+                <Button
+                  className="fixed bottom-5 left-5"
+                  onClick={() => {
+                    if (!useEditorStore.getState().sceneSnapshot) {
+                      createSnapshot();
+                    }
+                    setEditorOpen(true);
+                  }}
+                >
+                  Editor
+                </Button>
               )}
             </div>
-            {editorOpen || (
-              <Button
-                className="fixed bottom-5 left-5"
-                onClick={() => {
-                  if (!useEditorStore.getState().sceneSnapshot) {
-                    createSnapshot();
-                  }
-                  setEditorOpen(true);
-                }}
-              >
-                Editor
-              </Button>
-            )}
-          </div>
-          <Modal visible={stateMismatch}>
-            <ModalHeader>Saved state found</ModalHeader>
-            <ModalBody>
-              Would you like to use initial state or saved state?
-            </ModalBody>
-            <ModalFooter>
-              <Button
-                className="flex-1"
-                onClick={() => {
-                  applyPersistedState();
-                  setStateMismatch(false);
-                }}
-              >
-                Saved
-              </Button>
-              <Button
-                className="flex-1"
-                onClick={() => {
-                  setStateMismatch(false);
-                }}
-              >
-                Initial
-              </Button>
-            </ModalFooter>
-          </Modal>
-          <style type="text/css">{styles}</style>
-          <style>{'canvas { outline: none }'}</style>
-        </IdProvider>
-      </PortalManager>
+            <Modal visible={stateMismatch}>
+              <ModalHeader>Saved state found</ModalHeader>
+              <ModalBody>
+                Would you like to use initial state or saved state?
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  className="flex-1"
+                  onClick={() => {
+                    applyPersistedState();
+                    setStateMismatch(false);
+                  }}
+                >
+                  Saved
+                </Button>
+                <Button
+                  className="flex-1"
+                  onClick={() => {
+                    setStateMismatch(false);
+                  }}
+                >
+                  Initial
+                </Button>
+              </ModalFooter>
+            </Modal>
+            <style type="text/css">{styles}</style>
+            <style>{'canvas { outline: none }'}</style>
+          </IdProvider>
+        </PortalManager>
+      </div>
     </root.div>
   );
 };
