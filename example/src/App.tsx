dd46907165a8f579c9f789f9a11735124333be2a
Andrew Prifer <andrewpeterprifer@gmail.com>
2020-11-30 17:11:48 +0100

    Add CRA example

---
+++ example/src/App.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Canvas } from 'react-three-fiber';
+import { EditableManager, editable as e, configure } from '../../';
+import { PerspectiveCamera } from '@react-three/drei';
+
+const ECamera = e(PerspectiveCamera, 'perspectiveCamera');
+
+function App() {
+  return (
+    <Canvas shadowMap>
+      <EditableManager />
+      <ECamera makeDefault uniqueName="Camera" />
+      <e.spotLight
+        uniqueName="Key Light"
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+        castShadow
+      />
+      <e.spotLight
+        uniqueName="Fill Light"
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+        castShadow
+      />
+      <e.mesh uniqueName="Ground" receiveShadow>
+        <planeBufferGeometry />
+        <meshStandardMaterial color="lightblue" />
+      </e.mesh>
+      <e.group uniqueName="Ice Cream">
+        <e.mesh uniqueName="Cone" castShadow>
+          <coneBufferGeometry />
+          <meshStandardMaterial color="orange" />
+        </e.mesh>
+        <e.mesh uniqueName="Scoop 1" castShadow>
+          <sphereBufferGeometry />
+          <meshStandardMaterial color="red" />
+        </e.mesh>
+        <e.mesh uniqueName="Scoop 2" castShadow>
+          <sphereBufferGeometry />
+          <meshStandardMaterial color="green" />
+        </e.mesh>
+        <e.mesh uniqueName="Scoop 3" castShadow>
+          <sphereBufferGeometry />
+          <meshStandardMaterial color="blue" />
+        </e.mesh>
+      </e.group>
+    </Canvas>
+  );
+}
+
+export default App;
