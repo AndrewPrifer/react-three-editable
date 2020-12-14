fb6d3a87620421bf39f10295fbec20a626d1ce97
Andrew Prifer <andrewpeterprifer@gmail.com>
2020-12-14 19:11:50 +0100

    Move endOfLine: 'auto' fix to .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,16 @@
 module.exports = {
-  extends: [
-    'react-app',
-    'prettier/@typescript-eslint',
-  ],
+  extends: ['react-app', 'prettier/@typescript-eslint'],
   settings: {
     react: {
       version: 'detect',
     },
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
