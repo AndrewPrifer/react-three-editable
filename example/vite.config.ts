25d1d19f944e5632343aa3aeb2255e1b05044076
AriaMinaei <aria.minaei@gmail.com>
2021-05-16 12:50:40 +0200

    Use @theatre through a git dependency

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -12,4 +12,7 @@ export default defineConfig({
       root: '..',
     },
   },
+  optimizeDeps: {
+    include: ['@theatre/studio'],
+  },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Editor from './components/Editor';
+import studio from '@theatre/studio';
+
 export {
   default as EditorHelper,
   EditorHelperProps,
 } from './components/EditorHelper';
 export { default as editable } from './components/editable';
 export { EditableState, configure, BindFunction } from './store';
+
+console.log(studio);
 
 if (process.env.NODE_ENV === 'development') {
   const editorRoot = document.createElement('div');
